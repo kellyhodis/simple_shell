215669eef11919fd6b0e4f8739b849a2ddbfeced
Kelly Hodis <668@holbertonschool.com>
2019-04-06 22:34:17 +0000

    First commit

---
+++ supersimpleshell.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+int main(void)
+{
+	char *buffer = NULL, *token;
+	char *command[15];
+	size_t size = 1024;
+	int i = 0, status;
+	pid_t child_pid;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+		getline(&buffer, &size, stdin);
+	
+		token = strtok(buffer, "\n");
+		while (token != NULL)
+		{
+			command[i] = strdup(token);
+			printf("%s\n", command[i]);
+			i++;
+			token = strtok(NULL, "\n");
+		}
+		command[i] = NULL;
+
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			execve(command[0], command, NULL);
+			exit(0);
+		}
+		waitpid(child_pid, &status, 0);
+		buffer = NULL;
+		i = 0;
+	}
+	return (0);	
+}
