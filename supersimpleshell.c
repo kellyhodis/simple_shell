547ebccc4eb7470359855a05fd6d44a8e05a40b4
kellyhodis <kelly.hodis@protonmail.com>
2019-04-06 16:45:46 -0700

    Took write, getline, and tokens out of while loop

--- supersimpleshell.c
+++ supersimpleshell.c
@@ -9,24 +9,24 @@ int main(int argc, char **argv)
 	pid_t child_pid;
 
 	(void)argc;
+	write(STDOUT_FILENO, "#cisfun$ ", 9);
+	getline(&buffer, &size, stdin);
+
+	token1 = strtok(buffer, "\n");
+	token2 = strtok(token1, " ");
+	while (token2 != NULL)
+        {
+	        command[i] = strdup(token2);
+        	i++;
+        	token2 = strtok(NULL, "\n");
+        }
+        command[i] = NULL;
+	
 	while (1)
 	{
 		child_pid = fork();
 		if (child_pid == 0)
 		{
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
-			getline(&buffer, &size, stdin);
-
-			token1 = strtok(buffer, "\n");
-			token2 = strtok(token1, " ");
-			while (token2 != NULL)
-                	{
-	                        command[i] = strdup(token2);
-        	                i++;
-                	        token2 = strtok(NULL, "\n");
-                	}
-                	command[i] = NULL;
-
 			execve(command[0], command, NULL);
 			perror(argv[0]);
 			exit(0);
