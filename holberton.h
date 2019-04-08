4596f17b32704cb534725a1a38448be99fb97285
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-08 21:35:37 +0000

    added custom header file

---
+++ holberton.h
@@ -0,0 +1,29 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/resource.h>
+#include <errno.h>
+
+struct dir_s
+{
+	char *dir;
+	struct dir_s *next;
+}dir_s;
+
+char *_strncat(char *dest, char *src, int n);
+char *search_path(char *str);
+
+/* size_t **directory(dir_s **head, char **envp, mem_t *some);*/
+/* size_t *add_nodedir_end(dir_t **head, const char *dir);*/
+
+#endif 
