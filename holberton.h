ac14a3798df20645d942a5e8fb30f2b7a4058480
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-09 01:19:47 +0000

    fixed holberton header file

--- holberton.h
+++ holberton.h
@@ -1,10 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-<<<<<<< HEAD
-=======
-
->>>>>>> e73b0c9e9f3acf117c23c42c317a6871f877f408
-#include <unistd.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -16,7 +11,7 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <errno.h>
-
+#include <unistd.h>
 struct dir_s
 {
 	char *dir;
@@ -25,14 +20,7 @@ struct dir_s
 
 char *_strncat(char *dest, char *src, int n);
 char *search_path(char *str);
-
-/* size_t **directory(dir_s **head, char **envp, mem_t *some);*/
-/* size_t *add_nodedir_end(dir_t **head, const char *dir);*/
-
-<<<<<<< HEAD
+char *_getenv(const char *name);
+int _strcmp(char *s1, char *s2);
+void _env(char **environ);
 #endif 
-=======
-
-#endif /* HOLBERTON_H */
-
->>>>>>> e73b0c9e9f3acf117c23c42c317a6871f877f408
