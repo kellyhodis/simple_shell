62942aee72afb17662dfa772c6d34aedf7da318b
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-09 01:23:49 +0000

    function that acts as our prompt

--- _shell2.c
+++ _shell2.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
 	char *buffer = NULL, *token1, *token2;
 	char *command[15];
 	size_t size = 1024;
 	int i = 0, status, getEOF = 0;
 	pid_t child_pid;
-	int on = 1, flag = 1;
+	int on = 1, flag = 1, flag_1 = 1, exitcode;
 	struct stat st;
 	(void)argc;
 
@@ -31,17 +31,36 @@ int main(int argc, char **argv)
         		token2 = strtok(NULL, " ");
         	}
         	command[i] = NULL;
+		
+		if (_strcmp(command[0], "exit")== 0)
+			{
+				if (command[1] != NULL)
+					{
+						exitcode = atoi(command[1]);
+						exit(exitcode);
+					}
+					break;
+			}
+		if (_strcmp(command[0], "env") == 0)
+			{
+				flag_1 = 0;	
+				_env(envp);
+			}
+				
 		if (stat(command[0], &st)!= 0)
 			command[0] = search_path(command[0]);
-				
-		child_pid = fork();
-		if (child_pid == 0)
-		{
-			execve(command[0], command, NULL);
-			perror(argv[0]);
-			exit(0);
+		if (flag_1)
+		{	
+			child_pid = fork();
+			if (child_pid == 0)
+			{
+				execve(command[0], command, NULL);
+				perror(argv[0]);
+				exit(0);
+			}
+			waitpid(child_pid, &status, 0);
 		}
-		waitpid(child_pid, &status, 0);
+		flag_1 = 1;
 		buffer = NULL;
 		i = 0;
 		if (flag == 0)
