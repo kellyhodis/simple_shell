f18a7721134d97d92459cce8276163af003f418e
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-09 22:24:51 +0000

    updated main function

--- _shell2.c
+++ _shell2.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	char *buffer = NULL, *token1, *token2;
-	char *command[15];
+	char *buffer = NULL, *token2;
+	char *command[15], *lines[15];
 	size_t size = 1024;
-	int i = 0, status, getEOF = 0;
+	int i = 0, status, getEOF = 0, j = 0;
 	pid_t child_pid;
 	int on = 1, flag = 1, flag_1 = 1, exitcode;
 	struct stat st;
@@ -15,15 +15,23 @@ int main(int argc, char **argv, char **envp)
 	{
 		flag = 0;
 	}
-	
+		
+	write(STDOUT_FILENO, "#cisfun$ ", 9);
 	while (on)
 	{
-		write(STDOUT_FILENO, "#cisfun$ ", 9);
 		getEOF = getline(&buffer, &size, stdin);
 		if (getEOF == -1)
 			break;
-		token1 = strtok(buffer, "\n");
-		token2 = strtok(token1, " ");
+	lines[j] = strtok(buffer, "\n");
+	while (lines[j])
+	{
+		j++;
+		lines[j] = strtok(NULL, "\n");
+	}
+	j = 0;
+	while(lines[j])
+	{
+		token2 = strtok(lines[j]," ");
 		while (token2 != NULL)
 		{
 			command[i] = strdup(token2);
@@ -31,7 +39,6 @@ int main(int argc, char **argv, char **envp)
         		token2 = strtok(NULL, " ");
         	}
         	command[i] = NULL;
-		
 		if (_strcmp(command[0], "exit")== 0)
 			{
 				if (command[1] != NULL)
@@ -46,7 +53,7 @@ int main(int argc, char **argv, char **envp)
 				flag_1 = 0;	
 				_env(envp);
 			}
-				
+	/*_setenv("PATH", ": test: church: /bin/ls", 1);*/			
 		if (stat(command[0], &st)!= 0)
 			command[0] = search_path(command[0]);
 		if (flag_1)
@@ -60,11 +67,15 @@ int main(int argc, char **argv, char **envp)
 			}
 			waitpid(child_pid, &status, 0);
 		}
-		flag_1 = 1;
-		buffer = NULL;
+		j++;
 		i = 0;
+		flag_1 = 1;
+	}
+		j = 0;
+		buffer = NULL;
 		if (flag == 0)
 			on = 0;
+	write(STDOUT_FILENO, "#cisfun$ ", 9);
 	}
 	return (0);	
 }
