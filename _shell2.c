95c3a838c9ea700d34707dc6995bf9e89366cda2
Kelly Hodis <668@holbertonschool.com>
2019-04-10 23:15:59 +0000

    Removed memory changes

--- _shell2.c
+++ _shell2.c
@@ -2,17 +2,15 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	char *buffer = NULL, *token1 = NULL, *token2 = NULL;
+	char *buffer = NULL, *token1, *token2;
 	char *command[15], *lines[15];
 	size_t size = 1024;
 	int i = 0, status, getEOF = 0, j = 0;
 	pid_t child_pid;
-	int on = 1, piped = 1, flag_1 = 1, exitcode;
+	int on = 1, flag = 1, flag_1 = 1, exitcode;
 	struct stat st;
 	(void)argc;
 	
-	*command = _calloc(15,(sizeof(char *)));  
-	*lines = _calloc(15,(sizeof(char *)));
 	buffer = malloc(size);
 	if (buffer == NULL)
 		return ('\0');
@@ -20,7 +18,7 @@ int main(int argc, char **argv, char **envp)
 	{
 		read(STDIN_FILENO, buffer, size);
 		printf("%s\n", buffer);
-		piped = 0;
+		flag = 0;
 	lines[j] = strtok(buffer, "\n");
 	/* delimiter by line */
        		 while (lines[j])
@@ -35,33 +33,30 @@ int main(int argc, char **argv, char **envp)
 	write(STDOUT_FILENO, "#cisfun$ ", 9);
 	while (on)
 	{
-		if (piped !=0)
+		if (flag !=0)
 		{
 			getEOF = getline(&buffer, &size, stdin);
 			printf("%s\n", buffer);
-		if (getEOF == -1)
-			break;
-		lines[j] = "";
-		if (lines[j])
+			if (getEOF == -1)
+				break;
+			lines[j] = "";
+			if (lines[j])
 			{
-				/*lines[j] = malloc(strlen(buffer) + 1);*/
 				lines[j] = strdup(buffer); 
-				lines[j + 1] = NULL; 
+				lines[j + 1] = NULL;
 			}
 		}
 	while(lines[j])
-	{	
-		if (piped != 0)
+	{
+		if (flag != 0)
 		{
 			token1 = strtok(lines[j], "\n");
-			printf("%s\n", token1);
 		}
-		else
-		token1 = strtok(lines[j], "\0");
+		else	
+			token1 = strtok(lines[j], "\0");
 		token2 = strtok(token1," ");
 		while (token2 != NULL)
 		{
-			command[i] = malloc(strlen(token2) + 1);
 			command[i] = strdup(token2);
         		i++;
         		token2 = strtok(NULL, " ");
@@ -81,9 +76,8 @@ int main(int argc, char **argv, char **envp)
 				flag_1 = 0;	
 				_env(envp);
 			}
-	printf("%d\n",_setenv("PATH", ":test:church:/bin/ls", 1));			
-			if (stat(command[0], &st)!= 0)
-			command[0] = malloc(strlen(search_path(command[0])) + 1);
+	/*_setenv("PATH", ": test: church: /bin/ls", 1);*/			
+		if (stat(command[0], &st)!= 0)
 			command[0] = search_path(command[0]);
 			if (flag_1)
 			{	
@@ -102,7 +96,7 @@ int main(int argc, char **argv, char **envp)
 		}
 			j = 0;
 			buffer = NULL;
-			if (piped == 0)
+			if (flag == 0)
 				on = 0;
 	write(STDOUT_FILENO, "#cisfun$ ", 9);
 	}
