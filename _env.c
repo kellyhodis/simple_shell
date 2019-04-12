2eb2c1edf93577225dfffad50531de03a12d1794
Kelly Hodis <668@holbertonschool.com>
2019-04-12 03:32:28 +0000

    Replaced _env function to print environment

---
+++ _env.c
@@ -0,0 +1,18 @@
+#include "holberton.h" 
+
+/*
+* _env - prints the current environment
+* @environ: environment variable
+*
+* Returns: nothing
+*/
+
+void _env(char **environ)
+{
+	int i = 0;
+	while(environ[i]) 
+	{
+ 		 printf("%s\n", environ[i++]); 
+	}
+}
+
