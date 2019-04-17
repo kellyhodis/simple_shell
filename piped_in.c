87785d317dfbffee2aaff4713d5caefec71bd7e8
Kelly Hodis <668@holbertonschool.com>
2019-04-17 07:45:26 +0000

    Removed printf statements

--- piped_in.c
+++ piped_in.c
@@ -15,7 +15,6 @@ int piped_in(char *lines[], char *buffer)
 	if (!buffer)
 		return (1);
 	read(STDIN_FILENO, buffer, size);
-	printf("buffer: %s\n", buffer);
 	j = 0;
 	flag = 0;
 	lines[j] = strtok(buffer, "\n");
@@ -25,7 +24,5 @@ int piped_in(char *lines[], char *buffer)
 		j++;
 		lines[j] = strtok(NULL, "\n");
 	}
-	for (j = 0; lines[j]; j++)
-		printf("%s\n", lines[j]);
 	return (flag);
 }
