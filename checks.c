a03f39597609a22dfcf3c5a2b6c1c3235fb62d54
kellyhodis <668@holbertonschool.com>
2019-04-17 22:03:10 -0700

    Removed block comments

--- checks.c
+++ checks.c
@@ -24,19 +24,6 @@ int checks(char *command[], int *s_p, char *buf, int *f_t, int *ln, char **av)
 	if (stat(command[0], &st) != 0)
 	{
 		command[0] = search_path(command[0], s_p);
-/*		if (stat(command[0], &st) != 0)
-		{
-			count = _itoa(*ln);
-			execute_on = 0;
-			write(STDOUT_FILENO, av[0], _strlen(av[0]));
-			write(STDOUT_FILENO, ": ", 2);
-			write(STDOUT_FILENO, count, _strlen(count));
-			write(STDOUT_FILENO, ": ", 2);
-			write(STDOUT_FILENO, command[0], _strlen(command[0]));
-			write(STDOUT_FILENO, ": not found\n", 12);
-			free(count);
-		}
-*/	}
 	}
 	return (execute_on);
 }
