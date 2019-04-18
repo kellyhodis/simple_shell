46c3f4cfd595e103759eb7a0c5323e5f28bdbf9c
Kelly <668@holbertonschool.com>
2019-04-18 05:11:20 +0000

    Added an extra byte to write call

--- _env.c
+++ _env.c
@@ -11,7 +11,7 @@ void _env(void)
 
 	while (environ[i])
 	{
-		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]) + 1);
 		write(STDOUT_FILENO, "\n", 1);
 		i++;
 	}
